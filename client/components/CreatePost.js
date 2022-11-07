8eb091700b80de38d2038106934b913a783b9885
Rishi Ghosh <64273846+rishi4d@users.noreply.github.com>
2022-11-08 01:49:39 +0530

    New Posts handling

--- client/components/CreatePost.js
+++ client/components/CreatePost.js
@@ -1,15 +1,17 @@
 import { useSession } from 'next-auth/react';
 import { React, useRef, useState } from 'react';
 import Image from 'next/image';
-import { RiVideoAddFill } from "react-icons/ri";
+import { RiVideoAddFill, RiDeleteBin6Line } from "react-icons/ri";
 import { IoMdPhotos } from "react-icons/io";
 import { FaSmileBeam } from "react-icons/fa";
+import axios from 'axios';
 
 const CreatePost = () => {
   const { data: session } = useSession();
   const inputRef = useRef(null);
   const hiddenFileInput = useRef(null);
   const [imageToPost, setImageToPost] = useState(null);
+  const endpoint = "";
 
   const handleClick = () => {
     hiddenFileInput.current.click();
@@ -25,6 +27,32 @@ const CreatePost = () => {
     }
   }
 
+  const removeImage = () => {
+    setImageToPost(null);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(inputRef.current.value) return;
+    const formData = new FormData();
+
+    formData.append("file", imageToPost);
+    formData.append("post", inputRef.current.value);
+    formData.append("name", session?.user.name);
+    formData.append("email", session?.user.email);
+    formData.append("profilePic", session?.user.image);
+
+    axios.post(endpoint, formData, { headers : { Accept : "application/json"}})
+    .then((res) => {
+      inputRef.current.value = "";
+      removeImage;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+    
+  }
+
   return (
     <div className='bg-white rounded-lg shadow-md text-gray-500 p-2 divide-y'>
         <div className='flex p-4 space-x-2 items-center'>
@@ -32,11 +60,12 @@ const CreatePost = () => {
           <form className='flex flex-1'>
             <input className='rounded-full flex-grow h-12 focus:outline-none font-medium font-Poppins bg-gray-100 px-4' type="text" ref={inputRef} placeholder={`Whats on your mind, ${session?.user.name.split(" ")[0]}?`}></input>
           </form>
-          <button hidden></button>
+          <button hidden onClick={handleSubmit}></button>
         </div>
         {imageToPost && (
-          <div className='flex items-center px-4 py-2 space-x-4 cursor-pointer'>
+          <div onClick={removeImage} className='flex items-center px-4 py-2 space-x-4 cursor-pointer'>
             <img src={imageToPost} className='h-12 object-contain filter hover:brightness-110 transition duration-150' />
+            <RiDeleteBin6Line className='h-8 hover:text-red-500' />
           </div>
         )}
         <div className='flex justify-evenly pt-1'>
@@ -47,7 +76,7 @@ const CreatePost = () => {
           <div className='flex items-center p-1 space-x-1 rounded-lg flex-grow justify-center hover:bg-gray-100 cursor-pointer' onClick={handleClick}>
             <IoMdPhotos size={20} className="text-green-500" />
             <p className='font-semibold text-gray-600'>Photo/video</p>
-            <input onChange={addImageToPost} type="file" hidden ref={hiddenFileInput} accept="image/*"></input>
+            <input onChange={addImageToPost} type="file" multiple hidden ref={hiddenFileInput} accept="image/*"></input>
           </div>
           <div className='flex items-center p-1 space-x-1 rounded-lg flex-grow justify-center hover:bg-gray-100 cursor-pointer'>
             <FaSmileBeam size={20} className="text-yellow-500" />
