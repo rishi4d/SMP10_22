bac5ff2017c583df6ff67734bc6e2d05238e0c47
Rishi Ghosh <rishighosh1999@gmail.com>
2022-11-21 00:44:28 +0530

    checking in maven conf

---
+++ adda_service/src/test/java/com/rishi4d/adda_service/AddaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishi4d.adda_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
