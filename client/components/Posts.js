d73275373e3154b8f22c567315c3a205755912d6
Rishi Ghosh <64273846+rishi4d@users.noreply.github.com>
2022-12-10 22:39:05 +0530

    Service Changes#

--- client/components/Posts.js
+++ client/components/Posts.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectPost } from '../public/src/app/redux_store';
 import Post from './Post'
 
 const Posts = () => {
-  return <Post />
+  const dispatch = useDispatch();
+  const posts = useSelector(selectPost);
+  return (
+    <div>
+      {posts.map((post) => <Post post={post} key={post.id}/>)}
+    </div>
+  );
 }
 
 export default Posts
